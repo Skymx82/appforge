b879df553e8817d95a04a9f6034cfac574671967
Skymx82 <mattias.mathevon@gmail.com>
2025-01-29 10:25:16 +0100

    feat: amélioration de la sécurité et des en-têtes HTTP

--- next.config.js
+++ next.config.js
@@ -9,6 +9,23 @@ const nextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          }
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
